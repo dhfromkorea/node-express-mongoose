1f35b372b2066ed4519381fcd493324c8cd14328
dhfromkorea <dhllee424@gmail.com>
2014-07-08 10:04:10 -0700

    no mongoose for now

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 // Connect to mongodb
-var connect = function () {
-  var options = { server: { socketOptions: { keepAlive: 1 } } };
-  mongoose.connect(config.db, options);
-};
-connect();
+// var connect = function () {
+//   var options = { server: { socketOptions: { keepAlive: 1 } } };
+//   mongoose.connect(config.db, options);
+// };
+// connect();
 
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
+// mongoose.connection.on('error', console.log);
+// mongoose.connection.on('disconnected', connect);
 
 // Bootstrap models
 fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
